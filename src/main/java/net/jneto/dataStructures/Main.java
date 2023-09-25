b9a7ea4c7b3eb439b215607d61c403b8f4520ecf
Joao Neto <j.neto.rodrigues@hotmail.com>
2023-09-25 09:48:22 -0300

    ArrayList basic implement

--- src/main/java/net/jneto/dataStructures/Main.java
+++ src/main/java/net/jneto/dataStructures/Main.java
@@ -16,12 +16,31 @@ public class Main {
      * @param args - java -jar arguments
      */
     public static void main(String[] args) {
-        output("Stack");
+        /*output("Stack");
         stackDemonstration();
         output("Queue");
         queueDemonstration();
         output("Bag");
         bagDemonstration();
+        output("ArrayList");
+        listDemonstration();*/
+        ArrayList<String> test = new ArrayList<>();
+        test.add("a");
+        test.add("b");
+        test.add("c");
+        test.add("d");
+        test.add("e");
+        print(test);
+        print(test.show());
+        test.remove(1);
+        print(test.show());
+        print(test.remove("d"));
+        print(test.show());
+        ArrayList<Queue<String>> test2 = new ArrayList<>();
+        Queue<String> test3 = new Queue<String>();
+        test2.add(test3);
+        test2.peek();
+        print(test2.show());
     }
 
     private static void queueDemonstration() {
@@ -142,6 +161,49 @@ public class Main {
         print(structure.show());
         print(structure);
         structure.add("g");
+        structure.remove();
+        structure.remove();
+        structure.remove();
+        structure.remove();
+        print(structure);
+        structure.remove();
+        structure.remove();
+        structure.remove();
+        print(structure);
+    }
+
+    private static void listDemonstration(){
+        ArrayList<String> structure = new ArrayList<String>();
+        structure.add("a");
+        print(structure);
+        structure.add("a");
+        print(structure);
+        structure.add("a");
+        print(structure);
+        structure.add("a");
+        print(structure.show());
+        print(structure);
+        structure.add("b");
+        print(structure.show());
+        print(structure);
+        structure.add("c");
+        print(structure.show());
+        print(structure);
+        structure.add("d");
+        print(structure);
+        structure.add("e");
+        print(structure);
+        structure.add("f");
+        print(structure.show());
+        print(structure.showReverse());
+        print(structure.remove());
+        print(structure.remove());
+        print(structure.show());
+        print(structure.remove());
+        print(structure.remove());
+        print(structure.show());
+        print(structure);
+        structure.add("a");
         structure.remove();
         structure.remove();
         structure.remove();
