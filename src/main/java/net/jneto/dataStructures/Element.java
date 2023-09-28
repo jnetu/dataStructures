c8f45a7e009e1ac05cd1b26fdc5517636b521c6d
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-28 14:32:55 +0200

    Create Element.java

---
+++ src/main/java/net/jneto/dataStructures/Element.java
@@ -0,0 +1,13 @@
+/*
+* Element class that ist the basis for a lot of other classes for example LinkedLists
+*/
+
+public class Element() {
+    private Object value;
+    private Element next;
+    
+    public Element(Object value, Element next) {
+        this.value = value;
+        this.next = next;
+    }
+}
